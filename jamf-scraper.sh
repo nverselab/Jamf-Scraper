3dcc2cd48e717e7b935ea47b83d1a8206ddd9d77
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2023-02-19 18:13:10 -0600

    v0.3.0-alpha

--- jamf-scraper.sh
+++ jamf-scraper.sh
@@ -50,8 +50,8 @@
 # v0.1.0-a - 02/13/2023 - Initial WIP release to collect Computers, Devices, Jamf Accounts/Groups, 
 #                         Computer Groups, and Device Groups. - Jonathan Nelson
 # v0.2.0-a - 02/15/2023 - Restructured scripts into fragments to allow for only running enabled reports
-#                         and specifying an output directory (defaults to current user's Desktop). Also
-#                         added a Policies report.
+#                         and specifying an output directory (defaults to current user's Desktop)
+# v0.3.0-a - 02/19/2023 - Implimented prompts for variable input and report selections with IBM Notifier
 #
 ####################################################################################################
 #
@@ -85,15 +85,149 @@
 # Variables #
 #############
 
-# Base URL for Jamf Pro API endpoint
-base_url="https://yourJamfServer.jamfcloud.com/JSSResource"
+# Base URL for Jamf Pro API endpoint (Example: https://yourJamfProServer.jamfcloud.com/JSSResource)
+base_url=""
 
 # Jamf Pro API credentials
-api_username="auditorAPI"
-api_password="password"
+api_username=""
+api_password=""
 
 # Output Directory Path (if not specified, will default to current user's Desktop)
 outputDirectory=""
+
+# IBM Notifier Path
+IBMpath="./IBM\ Notifier.app/Contents/MacOS/IBM\ Notifier"
+
+
+##############################################################################################################
+#                                                                                                            #
+#                                     ! DO NOT MODIFY BELOW THIS LINE !                                      #
+#                                                                                                            #
+##############################################################################################################
+
+
+
+###############################################
+# Prompt for Jamf API details and output path #
+###############################################
+
+# Function to test Jamf Pro API credentials
+test_credentials() {
+	response=$(curl -sS -u "$api_username:$api_password" "$base_url/buildings" -H "Accept: application/xml" -X GET)
+	if [[ $response == *"401 Unauthorized"* ]]; then
+		echo "Invalid credentials. Please try again."
+		return 1
+	else
+		echo "The Jamf Pro API credentials are valid or an auth token already exists."
+		return 0
+	fi
+}
+
+# If API variables are empty, prompt for them
+
+if [ -z "$base_url" ]; then
+	# Prompt user for Jamf Pro base_url
+	IBMbase_url="-type popup -silent -title \"Jamf Scraper\" -accessory_view_type input -accessory_view_payload \"/placeholder https://yourJamfProServer.jamfcloud.com /title Please enter your Jamf Pro Server URL /required\" -main_button_label \"OK\""
+	
+	IBMcommand="$IBMpath $IBMbase_url"
+	
+	base_url=$(echo $IBMcommand | sh)
+	base_url="$base_url/JSSResource"
+fi
+
+if [ -z "$api_username" ]; then
+	# Prompt user for API_username
+	IBMapi_username="-type popup -silent -title \"Jamf Scraper\" -accessory_view_type input -accessory_view_payload \"/placeholder auditor /title Please enter your Jamf Pro API Username /required\" -main_button_label \"OK\""
+	
+	IBMcommand="$IBMpath $IBMapi_username"
+	
+	api_username=$(echo $IBMcommand | sh)
+fi
+
+if [ -z "$api_password" ]; then
+	# Prompt user for API_password (will not be visible)
+	IBMapi_password="-type popup -silent -title \"Jamf Scraper\" -accessory_view_type secureinput -accessory_view_payload \"/placeholder auditorPassword /title Please enter your Jamf Pro API password /required\" -main_button_label \"OK\""
+	
+	IBMcommand="$IBMpath $IBMapi_password"
+	
+	api_password=$(echo $IBMcommand | sh)
+fi
+
+# Test credentials
+test_credentials
+while [ $? -ne 0 ]; do
+	
+	# If API variables are empty, prompt for them
+	
+	if [ -z "$base_url" ]; then
+		# Prompt user for Jamf Pro base_url
+		IBMbase_url="-type popup -silent -title \"Jamf Scraper\" -accessory_view_type input -accessory_view_payload \"/placeholder https://yourJamfProServer.jamfcloud.com /title Please enter your Jamf Pro Server URL /required\" -main_button_label \"OK\""
+		
+		IBMcommand="$IBMpath $IBMbase_url"
+		
+		base_url=$(echo $IBMcommand | sh)
+		base_url="$base_url/JSSResource"
+	fi
+	
+	if [ -z "$api_username" ]; then
+		# Prompt user for API_username
+		IBMapi_username="-type popup -silent -title \"Jamf Scraper\" -accessory_view_type input -accessory_view_payload \"/placeholder auditor /title Please enter your Jamf Pro API Username /required\" -main_button_label \"OK\""
+		
+		IBMcommand="$IBMpath $IBMapi_username"
+		
+		api_username=$(echo $IBMcommand | sh)
+	fi
+	
+	if [ -z "$api_password" ]; then
+		# Prompt user for API_password (will not be visible)
+		IBMapi_password="-type popup -silent -title \"Jamf Scraper\" -accessory_view_type secureinput -accessory_view_payload \"/placeholder auditorPassword /title Please enter your Jamf Pro API password /required\" -main_button_label \"OK\""
+		
+		IBMcommand="$IBMpath $IBMapi_password"
+		
+		api_password=$(echo $IBMcommand | sh)
+	fi
+	
+	# Test credentials again
+	test_credentials
+done
+
+if [ -z "$outputDirectory" ]; then
+# Prompt user for output directory
+IBMoutputDirectory="-type popup -silent -title \"Jamf Scraper\" -accessory_view_type input -accessory_view_payload \"/placeholder \"path/to/output\" /title Where would you like to save your reports? /subtitle Leave this field blank to default to your Desktop\" -main_button_label \"OK\""
+	
+IBMcommand="$IBMpath $IBMoutputDirectory"
+	
+outputDirectory=$(echo $IBMcommand | sh)
+fi
+
+
+################################################
+# Build IBM Notifier Reports Secelction Prompt #
+################################################
+
+# Find all .sh files in the fragments subdirectory
+sh_files=($(find fragments -maxdepth 1 -type f -name '*.sh' -exec basename {} \;))
+sh_files=("${sh_files[@]#fragments/}")
+sh_files=("${sh_files[@]%.sh}")
+IBMarguments="-type popup -silent -title 'Jamf Scraper Report Selection' -accessory_view_type checklist -accessory_view_payload \"/list"
+
+for file in ${sh_files[@]}; do
+	echo "found script: $file"
+	IBMarguments=$IBMarguments" $file\n"
+done
+
+IBMarguments=$IBMarguments"\""
+IBMcommand="$IBMpath $IBMarguments"
+
+
+# Call IBM Notifier with generated payload
+result=$(echo $IBMcommand | sh)
+
+#############################
+# Generate Folder Structure #
+#############################
+
+# Create the XML and Reports folder if it doesn't exist
 
 # Check to see if outputDirectory is specified and if not default to user's Desktop
 if [[ -z "$outputDirectory" ]]; then
@@ -108,30 +242,34 @@ if [[ -z "$outputDirectory" ]]; then
 	outputDirectory=/Users/$currentUser/Desktop/jamfScraper-Results_$current_date
 fi
 
-# Get the current working directory
-script_dir=$(cd "$(dirname "$0")"; pwd)
-
-# Set arguments variable
-parameters="$base_url $api_username $api_password $outputDirectory"
-
-#############################
-# Generate Folder Structure #
-#############################
-
-# Create the XML and Reports folder if it doesn't exist
 if [ ! -d "$outputDirectory" ]; then
 	mkdir "$outputDirectory"
 	mkdir "$outputDirectory/XML"
 	mkdir "$outputDirectory/Reports"
 fi
 
-############################################################
-# Report Selection - Uncomment each report you want to run #
-############################################################
+########################
+# Run Selected Reports #
+########################
 
-#"$script_dir/fragments/computers.sh" $parameters
-#"$script_dir/fragments/devices.sh" $parameters
-#"$script_dir/fragments/jamf-accounts.sh" $parameters
-#"$script_dir/fragments/groups-computer.sh" $parameters
-#"$script_dir/fragments/groups-device.sh" $parameters
-#"$script_dir/fragments/policies.sh" $parameters
+# Set arguments variable
+parameters="$base_url $api_username $api_password $outputDirectory"
+
+# Parse the user's selections from the response
+IFS=' ' read -ra selected_files <<< "$result"
+for i in "${selected_files[@]}"; do    
+	echo "user selected: ./fragments/${sh_files[$i]}.sh"
+	sh ./fragments/${sh_files[$i]}.sh $parameters
+done
+
+#####################
+# Completion Prompt #
+#####################
+
+# Let the user know everything is finished and ask to open the reports folder
+IBMcomplete="-type popup -title \"Jamf Scrape Complete\" -subtitle \"Click OK to open your reports directory.\" -main_button_label \"OK\""
+IBMcommand="$IBMpath $IBMcomplete"
+complete=$(echo $IBMcommand | sh)
+open $outputDirectory/Reports
+
+exit 0
