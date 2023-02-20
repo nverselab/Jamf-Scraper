19e059d0a389ce24b153059489dc7aad9f743781
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2023-02-20 14:36:21 -0600

    added group memberships to computer/device tables

--- fragments/computers.sh
+++ fragments/computers.sh
@@ -43,7 +43,7 @@ fi
 curl -H "Accept: application/xml" -u "$api_username:$api_password" "$computers_endpoint" | xmllint --format - > $outputDirectory/XML/computers.xml
 
 # Create headers for the CSV file
-echo "Computer Name,Serial Number,Model,User,Department,Building,Room,OS Version,Enrolled Via DEP,MDM Capable,Last Check-In Time,Site" > $outputDirectory/Reports/computers_$current_date.csv
+echo "Computer Name,Serial Number,Model,User,Department,Building,Room,OS Version,Enrolled Via DEP,MDM Capable,Last Check-In Time,Site,Group Memberships" > $outputDirectory/Reports/computers_$current_date.csv
 
 # Get a list of all computer IDs
 computer_ids=`xmllint --xpath "//id/text()" $outputDirectory/XML/computers.xml | tr '\n' ' '`
@@ -64,6 +64,7 @@ for id in $computer_ids; do
 	mdm_capable=`echo "$computer_info" | xmllint --xpath "//computer/general/mdm_capable/text()" -`
 	os_version=`echo "$computer_info" | xmllint --xpath "//computer/hardware/os_version/text()" -`
 	model=`echo "$computer_info" | xmllint --xpath "//computer/hardware/model/text()" - | tr "," " "` 
-	echo "$computer_name,$serial_number,$model,$user,$department,$building,$room,$os_version,$enrolled_via_dep,$mdm_capable,$checkin_time,$site" >> $outputDirectory/Reports/computers_$current_date.csv
+	computer_groups=`echo "$computer_info" | xmllint --xpath "//computer/groups_accounts/computer_group_memberships/group/text()" - | tr '\n' ";" | tr "," " "`
+	echo "$computer_name,$serial_number,$model,$user,$department,$building,$room,$os_version,$enrolled_via_dep,$mdm_capable,$checkin_time,$site,$computer_groups" >> $outputDirectory/Reports/computers_$current_date.csv
 	echo $computer_info | xmllint --format - > $outputDirectory/XML/Computers/computerID_$id-SN_$serial_number.xml
 done
